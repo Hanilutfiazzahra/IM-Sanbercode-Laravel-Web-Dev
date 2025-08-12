0bfb90216dfa2622f2418fc33ee143b182308f38
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2025-08-12 18:22:18 +0700

    latihan php string dan array

---
+++ php-1/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Berlatih PHP</title>
+</head>
+<body>
+    <?php
+        $hello = "Hai Hani! Aku adalah PHP, bahasa pemrograman yang kamu pelajari sekarang. Aku sangat menyenangkan dan mudah dipelajari. Selamat belajar!";
+        echo "<h1>$hello</h1>";
+    ?>
+</body>
+</html>
