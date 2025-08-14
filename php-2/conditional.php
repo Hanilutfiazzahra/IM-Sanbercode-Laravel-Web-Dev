d1cc40ff386fe564f3b8267d738f2523b42c5ca9
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2025-08-14 11:25:49 +0700

    latihan looping dan conditional

---
+++ php-2/conditional.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Function</title>
+</head>
+
+<body>
+<h1>Berlatih Function PHP</h1>
+<?php
+
+echo "<h3> Soal No 1 Greetings </h3>";
+
+function greetings($name) {
+    echo "Halo " . $name . ", Selamat Datang di Sanbercode!<br>";
+}
+
+greetings("Bagas");
+greetings("Wahyu");
+greetings("nama peserta");
+
+echo "<br>";
+
+echo "<h3>Soal No 2 Reverse String</h3>";
+
+function reverse($kata1) {
+    $panjangkata = strlen($kata1);
+    $tampung = "";
+    for ($i = ($i = $panjangkata - 1); $i >= 0; $i--) {
+        $tampung .= $kata1[$i];
+    }
+    return $tampung;
+}
+
+function reverseString ($kata2) {
+    $string = reverse($kata2);
+    echo "Kata Asli: " . $kata2 . "<br>";
+    echo "Kata Terbalik: " . $string . "<br> <br>";
+}
+
+reverseString("nama peserta");
+reverseString("Sanbercode");
+reverseString("We Are Sanbers Developers");
+
+echo "<h3>Soal No 3 Palindrome </h3>";
+
+function palindrome($str) {
+    $str = strtolower($str); 
+    $length = strlen($str);
+    $reversed = "";
+
+    for ($i = $length - 1; $i >= 0; $i--) {
+        $reversed .= $str[$i];
+    }
+
+    if ($reversed === $str) {
+        echo $str . " : true<br>";
+        return true;
+    } else {
+        echo $str . " : false<br>";
+        return false;
+    }
+}
+
+palindrome("civic");   
+palindrome("nababan"); 
+palindrome("jambaban"); 
+palindrome("racecar");  
+
+echo "<h3>Soal No 4 Tentukan Nilai </h3>";
+
+function tentukan_nilai($nilai) {
+    if ($nilai >= 85 && $nilai <= 100) {
+        return $nilai . " : Sangat Baik <br>";
+    } elseif ($nilai >= 70 && $nilai < 85) {
+        return $nilai . " : Baik <br>";
+    } elseif ($nilai >= 60 && $nilai < 70) {
+        return $nilai . " : Cukup <br>";
+    } else {
+        return $nilai . " : Kurang <br>";
+    }
+}
+
+echo tentukan_nilai(98);
+echo tentukan_nilai(76); 
+echo tentukan_nilai(67); 
+echo tentukan_nilai(43); 
+
+
+?>
+
+</body>
+
+</html>
---
+++ php-2/looping.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Looping</title>
+
+</head>
+
+<body>
+
+    <h1>Berlatih Looping</h1>    
+     <?php 
+
+        echo "<h3>Soal No 1 Looping I Love PHP</h3>";
+
+        echo "<h4> LOOPING PERTAMA <br> </h2>";
+      
+        for($i = 2; $i < 21 ; $i+=2 ){
+        echo $i . " - I LOVE PHP <br> <br>";
+        }
+
+        echo "<h4> LOOPING KEDUA <br> </h2>";
+
+        for($i = 20; $i > 1 ; $i-=2 ){
+        echo $i . " - I LOVE PHP <br> <br>";
+        }
+
+        echo "<h3>Soal No 2 Looping Array Modulo </h3>";
+
+        $numbers = [18, 45, 29, 61, 47, 34];
+
+        echo "array numbers: ";
+
+        print_r($numbers);
+       
+        echo "<br>";
+
+        echo "Array sisa baginya adalah:  "; 
+        foreach ($numbers as $value) {
+            $rest[] = $value % 5;
+        }
+        print_r($rest);
+        echo "<br>";      
+
+        echo "<h3> Soal No 3 Looping Asociative Array </h3>";
+
+        $items = [
+
+            ['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpeg'], 
+
+            ['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mekanik', 'msi.jpeg'],
+
+            ['003', 'Mouse Genius', 50000, 'Mouse Genius biar lebih pinter', 'genius.jpeg'],
+
+            ['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', 'jerry.jpeg']
+
+        ];
+
+        foreach ($items as $key => $value) {
+            $items = [
+                'id' => $value[0],
+                'name' => $value[1],
+                'price' => $value[2],
+                'description' => $value[3],
+                'source' => $value[4]
+            ];
+            print_r($items);
+            echo "<br> <br>";
+        }
+
+        echo "<h3>Soal No 4 Asterix </h3>";
+
+        echo "Asterix: ";
+
+        echo "<br>";
+
+        for ($i = 1; $i <= 5; $i++) {
+            for ($j = 1; $j <= $i; $j++) {
+                echo "* ";
+            }
+            echo "<br>";
+        }
+
+        echo "<br>";        
+
+    ?></body>
+
+</html>
